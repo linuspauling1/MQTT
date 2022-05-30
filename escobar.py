9b8123c426f73b929daf39faa92511b94875a800
linuspauling1 <paulb2233@yahoo.com>
2022-05-30 21:21:34 +0300

    Schimbare de canal

--- escobar.py
+++ escobar.py
@@ -3,14 +3,16 @@ import time
 import mysql.connector as conexiune
 from queue import Queue
 
-q1 = Queue()
-q2 = Queue()
+q1 = Queue() #contine datele primite de la wifi1
+q2 = Queue() #contine datele primite de la wifi2
+q3 = Queue() #contine datele primite de la 1-wire
 cliend_id = 'rpi2' #un client asemanator cu cel anterior
 clean_session = False #dorim ca mesajele sa fie pastrate pana la reconectare
 host = '101.232.174.243'
 port_insecure = 1883 #portul securizat fiind 8883
 qos = 2 #calitatea maxima
-
+buffer_size = 100 #dimensiunea circular buffer-ului MySQL
+#stabilim o conexiune cu baza de date mysql
 db = conexiune.connect(
     host = host,
     user = 'root',
@@ -18,36 +20,49 @@ db = conexiune.connect(
     database = 'bazaDeDate'
 )
 cursorul_meu = db.cursor()
-
+#numele canalelor:
+topic_1wire = 'temperaturi/1wire'
 topic_wifi_senzor1 = 'temperaturi/wifi/senzor1'
 topic_wifi_senzor2 = 'temperaturi/wifi/senzor2'
 topic_wifi = 'temperaturi/wifi/#'
-
+#callbacks:
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         client.connected_flag = True
-        print('M-am conectat.')
+        print('M-am conectat.') #daca return code-ul este nul, atunci conexiunea este viabila
     else:
         client.bad_connection_flag = True
-        print('Eroare la conexiune cu codul: ',rc)
-    try:
+        print('Eroare la conexiune cu codul: ',rc) #altfel nu
+    try: #incercam sa ne abonam la canalele pentru senzorii wifi
         (result, mid) = client.subscribe(topic_wifi, qos)
-        while result != 0:
+        while result != 0: #daca rezultatul este nul atunci conexiunea este slaba si trebuie refacuta
             print('... ...')
             (result, mid) = client.subscribe(topic_wifi, qos)
         print('Am reusit abaonarea la canlul cu identificatorul de mesaj: ', mid)
-    except:
+    except: #daca primim o excpetie trebuie sa iesim din program intrucat sunt parametri incorecti
         print('Nu am putut sa ne abonam la canalul cu datele primite de la senzori...')
         exit(-3)
+    try: #incercam sa ne abonam la canalul pentru senzorul 1-wire
+        (result_1wire, mid_1wire) = client.subscribe(topic_1wire, qos)
+        while result_1wire != 0:
+            print('... ... ...')
+            (result_1wire, mid_1wire) = client.subscribe(topic_1wire, qos)
+        print('Am reusit abaonarea la canlul pentru senzorul 1-wire cu identificatorul de mesaj: ', mid)
+    except:
+        print('Nu am putut sa ne abonam la canalul cu datele primite de la senzorul 1wire...')
+        exit(-3)
 def on_disconnect(client, userdata, rc):
-    print('Clientul s-a deconectat cu codul: ', rc)
-    if rc < 7:
-        client.connected_flag = False
-        client.loop_stop()
-        print('Este o eroare grava...')
-        exit(-4)
+    if rc == 0:
+        print('Clientul s-a deconectat...Totul este bine.')
+    else:
+        print('Clientul s-a deconectat cu codul: ', rc,' ... Incercam reconectarea...')
+        if rc < 7:
+            client.connected_flag = False
+            client.loop_stop()
+            print('Clientul s-a deconectat cu codul: ',rc,' ... Este o eroare grava ...')
+            exit(-4)
 def on_subscribe(client, userdata, mid, granted_qos):
-    print('Identificaoturl de mesaj emis : ',mid)
+    print('Identificaoturl de mesaj emis : ',mid) #trebuie sa corespunda mid-urile
     pass
 def on_message(client, userdata, message):
     payload = str(message.payload.decode("utf-8"))
@@ -57,13 +72,15 @@ def on_message(client, userdata, message):
     elif message.topic == topic_wifi_senzor2:
         print('De la wifi2 avem: ',payload)
         q2.put(payload)
+    elif message.topic == topic_1wire:
+        print('De la 1-wire avem: ', payload)
+        q3.put(payload)
     if message.retain == 1:
         print("Mesajul acesta a fost emis cat timp clientul era deconectat...")
 
 client = mqtt.Client(cliend_id,clean_session)
 client.bad_connection_flag = False #flag pentru conexiune deficitara
 client.connected_flag = False #flag pentru conexiune stabilita
-client.suback_flag = False #flag pentru subscriere
 client.on_connect = on_connect
 client.on_disconnect = on_disconnect
 client.on_subscribe = on_subscribe
@@ -80,24 +97,24 @@ while not client.connected_flag and not client.bad_connection_flag:
 if client.bad_connection_flag:
     client.loop_stop()
     exit(-2)
-while client.connected_flag or client.bad_connection_flag:
+while not client.bad_connection_flag:
     print('q1 are dimensiunea: ',q1.qsize(),' si q2: ',q2.qsize())
-    while not q1.empty() and not q2.empty():
-        (t1,t2) = (q1.get(),q2.get()) #puteam nota direct parametri, insa este mai elegant asa
+    while not q1.empty() and not q2.empty(): # and not q3.empty()
+        (t1,t2) = (q1.get(),q2.get()) #,q3.get()) # indiferent de succesul operatiunilor, vom muta in permanenta elementele din cozi - sunt de timp real
         try:
-            params = [100] #valoarea buffer-ului mysql va fi 100
+            params = [buffer_size]
             try:
                 cursorul_meu.callproc('adaugare',params)
             except:
                 print('Nu merge adaugarea, deci sunt parametri gresiti...')
-                time.slepp(1)
-                continue
-            try:
-                cursorul_meu.execute('insert into temperaturi(nume_camera, temperatura_wifith1, temperatura_wifith2) values(\'albastra\',%s, %s)',(t1,t2))
+                time.sleep(1)
+                break
+            try: #nu va ramane asa, trebuie citit si senzorul 1-wire
+                cursorul_meu.execute('insert into temperaturi(nume_camera, temperatura_wifith1, temperatura_wifith2) values(\'albastra\',%s, %s %s)',(t1,t2,'25'))
             except:
                 print('Insertie defectuaosa, deci sunt parametri gresiti...')
                 time.sleep(1)
-                continue
+                break 
             db.commit()
         except:
             print('Nu exista inca baza de date!!!')
@@ -112,6 +129,7 @@ while client.connected_flag or client.bad_connection_flag:
                 cursorul_meu = db.cursor()
             except:
                 print('Reconectare esuata...')
+                break
     time.sleep(1) #perioada este diferita de cea pentru achizitia unei temepraturi
 cursorul_meu.close()
 client.loop_stop()
--- pablo.py
+++ pablo.py
@@ -18,7 +18,6 @@ mydb = mysql.connector.connect( #ne conectam la baza de date
 my_cursor=mydb.cursor() #generam o instanta pentru un cursor
 
 topic_perioada = 'nodemcu/perioada'
-topic_temperatura_medie = 'nodemcu/medie'
 topic_prag_inferior = 'nodemcu/prag/inferior'
 topic_prag_superior = 'nodemcu/prag/superior'
 topic_diferenta = 'nodemcu/diferenta'
@@ -42,12 +41,15 @@ def on_connect(client, userdata, flags, rc):
         client.bad_connection_flag = True #setam fanionul ca sa putem iesi din script
         print('Conexiunea a esuat cu codul: ', rc)
 def on_disconnect(client, userdata, rc):
-    print('Clientul s-a deconectat cu codul: ', rc)
-    if rc < 7:
-        client.connected_flag = False
-        client.loop_stop()
-        print('Este o eroare grava...')
-        exit(-4)
+    if rc == 0:
+        print('Clientul s-a deconectat... Totul este bine.')
+    else:
+        print('Clientul s-a deconectat cu codul: ', rc,' ... Incercam reconectarea...')
+        if rc > 0 and rc < 7:
+            client.connected_flag = False
+            client.loop_stop()
+            print('Clientul s-a deconectat cu codul ',rc,' ... Este o eroare grava ...')
+            exit(-4)
 def on_publish(client, userdata, result):
     print('Mesajul a fost trimis cu codul ', result)
 
@@ -69,89 +71,79 @@ while not client.bad_connection_flag and not client.connected_flag: #cat timp nu
 if client.bad_connection_flag:
     client.loop_stop() #incheiem bucla de procesare a evenimentelor
     exit(-2) #iesim fortat
-client.loop_stop() #oprim firul buclei intrucat o vom apela periodic
-while client.connected_flag or client.bad_connection_flag:
-    try:
-        my_cursor.execute('select perioada from camere where nume_camera = \'albastra\'')#achizitie perioada
+#client.loop_stop() #oprim firul buclei intrucat o vom apela periodic
+while not client.bad_connection_flag:
+    #client.loop() #apelam bucla pentru fiecare ciclare
+    if client.connected_flag:
         try:
-            perioada = my_cursor.fetchall()[0][0]
+            mydb.commit()
+            my_cursor.execute('select perioada from camere where nume_camera = \'albastra\'')#achizitie perioada
+            try:
+                perioada = my_cursor.fetchall()[0][0]
+                if(perioada is None):
+                    print('Informatiile despre camere sunt absente!!!')
+                else:
+                    perioada = int(perioada)
+                    if ante_perioada != perioada:
+                        client.publish(topic_perioada, perioada, qos, retain)
+                        ante_perioada = perioada
+                        print('Noua perioada: ',perioada)
+            except:
+                print('Nu exista inca date din tabela camerelor!!!')
+            my_cursor.execute('select prag_inferior from camere where nume_camera = \'albastra\'')#achizitie prag inferior
+            try:
+                prag_inferior = my_cursor.fetchall()[0][0]
+                if(prag_inferior is None):
+                    print('Informatiile despre camere sunt absente - perioada!!!')
+                else:
+                    prag_inferior = float(prag_inferior)
+                    if ante_prag_inferior != prag_inferior:
+                        client.publish(topic_prag_inferior, prag_inferior, qos, retain)
+                        ante_prag_inferior = prag_inferior
+                        print('Noul prag inferior: ',prag_inferior)
+            except:
+                print('Nu exista inca date din tabela camerelor - perioada!!!')
+            my_cursor.execute('select prag_superior from camere where nume_camera = \'albastra\'')#achizitie prag superior
+            try:
+                prag_superior = my_cursor.fetchall()[0][0]
+                if(prag_superior is None):
+                    print('Informatiile despre camere sunt absente - prag superior!!!')
+                else:
+                    prag_superior = float(prag_superior)
+                    if ante_prag_superior != prag_superior:
+                        client.publish(topic_prag_superior, prag_superior, qos, retain)
+                        ante_prag_superior = prag_superior
+                        print('Noul prag superior: ',prag_superior)
+            except:
+                print('Nu exista inca date din tabela camerelor - prag superior!!!')
+            my_cursor.execute('select diferenta from camere where nume_camera = \'albastra\'')#achizitie diferenta
+            try:
+                diferenta = my_cursor.fetchall()[0][0]
+                if(diferenta is None):
+                    print('Informatiile despre camere sunt absente - diferenta temperaturi terminstori !!!')
+                else:
+                    diferenta = float(diferenta)
+                    if ante_diferenta != diferenta:
+                        client.publish(topic_diferenta, diferenta, qos, retain)
+                        ante_diferenta = diferenta
+                        print('Noua diferenta: ',diferenta)
+            except:
+                print('Nu exista inca date din tabela camerelor - diferenta temperaturi termistori !!!')
+            mydb.commit()
         except:
-            print('Nu exista inca date din tabela camerelor!!!')
-        if(perioada is None):
-            print('Informatiile despre camere sunt absente!!!')
-        else:
-            perioada = int(perioada)
-        my_cursor.execute('select prag_inferior from camere where nume_camera = \'albastra\'')#achizitie prag inferior
-        try:
-            prag_inferior = my_cursor.fetchall()[0][0]
-        except:
-            print('Nu exista inca date din tabela camerelor - perioada!!!')
-        if(prag_inferior is None):
-            print('Informatiile despre camere sunt absente - perioada!!!')
-        else:
-            prag_inferior = float(prag_inferior)
-        my_cursor.execute('select prag_superior from camere where nume_camera = \'albastra\'')#achizitie prag superior
-        try:
-            prag_superior = my_cursor.fetchall()[0][0]
-        except:
-            print('Nu exista inca date din tabela camerelor - prag superior!!!')
-        if(prag_superior is None):
-            print('Informatiile despre camere sunt absente - prag superior!!!')
-        else:
-            prag_superior = float(prag_superior)
-        my_cursor.execute('select diferenta from camere where nume_camera = \'albastra\'')#achizitie diferenta
-        try:
-            diferenta = my_cursor.fetchall()[0][0]
-        except:
-            print('Nu exista inca date din tabela camerelor - diferenta temperaturi termistori !!!')
-        if(diferenta is None):
-            print('Informatiile despre camere sunt absente - diferenta temperaturi terminstori !!!')
-        else:
-            diferenta = float(diferenta)
-        my_cursor.execute('select temperatura_medie from temperaturi where nume_camera = \'albastra\' and indice = 1') #achizitie temperatura medie
-        try:
-            temperatura_medie = my_cursor.fetchall()[0][0]
-        except:
-            print('Nu exista inca date - temperatura medie !!!')
-        if(temperatura_medie is None):
-            print('Informatia este absenta - temperatura medie !!!')
-        else:
-            temperatura_medie = float(temperatura_medie)
-        mydb.commit()
-    except:
-        print('Nu exista inca baza de date!!!')
-        time.sleep(1)
-        try:
-            mydb = mysql.connector.connect(
-                host = '101.232.174.243',
-                user = 'root',
-                passwd = 'prikoke',
-                database = 'bazaDeDate'
-            )
-            my_cursor = mydb.cursor()
-        except:
-            print('Reconectare esuata...')
-    if ante_perioada != perioada:
-        client.publish(topic_perioada, perioada, qos, retain)
-        ante_perioada = perioada
-        print('Noua perioada: ',perioada)
-    if ante_prag_inferior != prag_inferior:
-        client.publish(topic_prag_inferior, prag_inferior, qos, retain)
-        ante_prag_inferior = prag_inferior
-        print('Noul prag inferior: ',prag_inferior)
-    if ante_prag_superior != prag_superior:
-        client.publish(topic_prag_superior, prag_superior, qos, retain)
-        ante_prag_superior = prag_superior
-        print('Noul prag superior: ',prag_superior)
-    if ante_diferenta != diferenta:
-        client.publish(topic_diferenta, diferenta, qos, retain)
-        ante_diferenta = diferenta
-        print('Noua diferenta: ',diferenta)
-    if ante_temperatura_medie != temperatura_medie:
-        client.publish(topic_temperatura_medie, temperatura_medie, qos, retain)
-        ante_temperatura_medie = temperatura_medie
-        print('Noua temperatura medie: ',temperatura_medie)
-    client.loop() #apelam bucla pentru fiecare ciclare
+            print('Nu exista inca baza de date!!!')
+            time.sleep(1)
+            try:
+                mydb = mysql.connector.connect(
+                    host = '101.232.174.243',
+                    user = 'root',
+                    passwd = 'prikoke',
+                    database = 'bazaDeDate'
+                )
+                my_cursor = mydb.cursor()
+            except:
+                print('Reconectare esuata...')
     time.sleep(perioada/1000) #perioada este aceeasi ca pentru achizitia temperaturii
+client.loop_stop() #oprim bucla
 my_cursor.close() #nu mai folosim cursorul
 client.disconnect() #ne deconectam
